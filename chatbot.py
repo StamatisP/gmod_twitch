1926ee85a0cbe3d3b2da2236e7da9da2a38c4909
Stamos Prekas <prekasstamos@gmail.com>
2019-09-19 14:43:32 -0400

    unneeded stuff

--- chatbot.py
+++ chatbot.py
@@ -5,23 +5,6 @@ import websockets
 import time
 import os
 import sys
-import psutil
-import logging
-
-def restart_program():
-    """Restarts the current program, with file objects and descriptors
-       cleanup
-    """
-
-    try:
-        p = psutil.Process(os.getpid())
-        for handler in p.get_open_files() + p.connections():
-            os.close(handler.fd)
-    except Exception as e:
-        logging.error(e)
-
-    python = sys.executable
-    os.execl(python, python, *sys.argv)
 
 global inc_msg
 inc_msg = "null"
